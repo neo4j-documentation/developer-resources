fae9d4f8a68cbe742b6c39c95e21ef560af901ad
Mark Needham <mark.needham@neotechnology.com>
2014-09-05 15:06:32 +0100

    putting domain model image here

--- render.sh
+++ render.sh
@@ -3,6 +3,6 @@ for file in `find . -name "*.adoc" -not -name README.adoc`; do
    asciidoctor -a source-highlighter=codemirror -T asciidoc $file -o ${file%/*}/index.html
 done
 
-git add .
-git commit -m "content-update for github-pages"
-git push origin gh-pages
+#git add .
+#git commit -m "content-update for github-pages"
+#git push origin gh-pages
