f061a8583ee08b8806e5b53556735398b2a63c0f
Julian Simpson <simpsonjulian@gmail.com>
2014-09-19 15:51:30 -0700

    Bundler support for deploy.sh

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 gem 'asciidoctor', '~> 1.5.1'
 gem 'rubypress'
+gem 'tilt'
+gem 'thread_safe'
--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ for file in `find . -mindepth 2 -name "*.adoc"`; do
    echo "Rendering $file"
    filename=${file##*/}
    filename=${filename%.adoc}
-   asciidoctor -a source-highlighter=codemirror -T _templates/wordpress $file -o deploy/${filename}.html
+   bundle exec asciidoctor -a source-highlighter=codemirror -T _templates/wordpress $file -o deploy/${filename}.html
 done
 
 #git add .
