0aa104360ca6fda8bf8bf124548c8135ffe0531d
Mark Needham <mark.needham@neotechnology.com>
2014-09-20 08:51:25 +0100

    adding keywords + getting all movies for other things

--- scraping/movies_detailed.rb
+++ scraping/movies_detailed.rb
@@ -4,13 +4,11 @@ require 'csv'
 
 API_KEY = ENV['MOVIE_DB_KEY']
 
-#http://api.themoviedb.org/3/movie/194?api_key=926d2a79e82920b62f03b1cb57e532e6&append_to_response=casts
-
-CSV.open("all-genres.csv2", 'wb') do |f|
+CSV.open("all-genres.csv", 'wb') do |f|
   f << ["MovieId", "GenreId", "GenreName"]
 end
 
-CSV.open("all-casts.csv2", 'wb') do |f|
+CSV.open("all-casts.csv", 'wb') do |f|
   f << ["MovieId", "CastId", "Character", "ActorId", "ActorName", "Order"]
 end
 
@@ -21,13 +19,13 @@ CSV.foreach("all-movies.csv", :headers => true) do  |row|
 
   p result
 
-  CSV.open("all-genres.csv2", 'ab') do |f|
+  CSV.open("all-genres.csv", 'ab') do |f|
       result["genres"].each do |row|
         f << [movie_id, row["id"], row["name"]]
       end
   end
 
-  CSV.open("all-casts.csv2", 'ab') do |f|
+  CSV.open("all-casts.csv", 'ab') do |f|
     result["casts"]["cast"].each do |row|
       f << [movie_id, row["cast_id"], row["character"], row["id"], row["name"], row["order"]]
     end
