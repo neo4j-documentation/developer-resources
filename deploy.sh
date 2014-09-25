9b98fe70fa75373cd9e2ef92496fe37a0c58d6df
Michael Hunger <github@jexp.de>
2014-09-26 00:32:52 +0200

    added {img} prefix for images and imaginary wordpress image-base-url to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 rm -rf deploy
 mkdir deploy
 
+export IMAGE_BASE_URL=http://dev.assets.neo4j.com.s3.amazonaws.com/wp-content/uploads/
+
 for file in `find . -mindepth 2 -name "*.adoc"`; do
    echo "Rendering $file"
    filename=${file##*/}
    filename=${filename%.adoc}
-   bundle exec asciidoctor -a source-highlighter=codemirror -T _templates/wordpress $file -o deploy/${filename}.html
+   bundle exec asciidoctor -a source-highlighter=codemirror -a img=${IMAGE_BASE_URL} -T _templates/wordpress $file -o deploy/${filename}.html
 done
 ./publish.rb 'deploy/guide-build-a-recommendation-engine.html'
 
