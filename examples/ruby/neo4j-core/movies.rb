9fc78d7a5fa4b06b06a6cc29af3b2e7692b714ac
Andreas Ronge <andreas.ronge@gmail.com>
2014-09-26 23:55:59 +0200

    neo4j-core example, implemented /graph resource
     Copied from py2neo

--- examples/ruby/neo4j-core/movies.rb
+++ examples/ruby/neo4j-core/movies.rb
@@ -1,21 +1,42 @@
 require 'sinatra'
-
 require 'neo4j-core'
-set :public_folder, File.dirname(__FILE__) + '/static'
-
+set :root, File.dirname(__FILE__)
 
 session = Neo4j::Session.open
+
 get '/' do
-  redirect to('/index.html')
+  redirect '/index.html'
 end
 
 get '/graph' do
-  query = "MATCH (m:Movie)<-[:ACTED_IN]-(a:Person) " +
-  "RETURN m.title as movie, collect(a.name) as cast " +
-  "LIMIT 10"
+  puts "QUERY"
+  query = """
+    MATCH (m:Movie)<-[:ACTED_IN]-(a:Person)
+    RETURN m.title as movie, collect(a.name) as cast
+    LIMIT {limit}
+  """
 
-  result = session.query(query).map(&:cast)
+  movies_and_casts = session.query(query, limit: params[:limit] || 50)
 
-  puts "RESULT #{result}"
-  result.to_json
+  nodes = []
+  rels = []
+  i = 0
+  movies_and_casts.each do |row|
+    nodes << {title: row.movie, label: 'movie'}
+    target = i
+    i += 1
+    row.cast.each do |name|
+      actor = {title: name, label: "actor"}
+      source = nodes.index(actor)
+      unless source
+        source = i
+        nodes << actor
+        i+=1
+      end
+      rels << {source: source, target: target}
+    end
+
+  end
+  {nodes: nodes, links: rels}.to_json
+
 end
