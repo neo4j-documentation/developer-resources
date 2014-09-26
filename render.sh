10f9830d340ddcf0855772785c73e306ac8b42e6
Michael Hunger <github@jexp.de>
2014-09-26 23:47:19 +0200

    updated render.sh to also render single files

--- render.sh
+++ render.sh
@@ -1,7 +1,17 @@
-for file in `find . -mindepth 2 -name "*.adoc"`; do
-   echo "Rendering $file"
-   asciidoctor -a source-highlighter=codemirror -a linkattrs -a img=./ -T _templates $file -o ${file%/*}/index.html
-done
+function render {
+   file="$1"
+   to=${file%/*}/index.html
+   echo "Rendering $file to $to"
+   asciidoctor -a source-highlighter=codemirror -a linkattrs -a img=./ -T _templates $file -o $to
+}
+
+if [ "$1" != "" ]; then
+   render "$1"	
+else
+   for file in `find . -mindepth 2 -name "*.adoc"`; do
+    	render "$file"
+   done
+fi
 
 #git add .
 #git commit -m "content-update for github-pages"
