10e5d9ad6f6b36b291714434239c0fb73da0ff3f
Brian Underwood <ml+github@semi-sentient.com>
2014-09-30 13:19:40 +0200

    Forgot to update the models previously

--- examples/ruby/neo4jrb/models.rb
+++ examples/ruby/neo4jrb/models.rb
@@ -1,17 +1,17 @@
+
 class Movie
   include Neo4j::ActiveNode
   id_property :title
-  has_many :in, :actors, type: :ACTED_IN, model_class: :Person
-end
-
-class Engagement
-  include Neo4j::ActiveRel
-  property :roles
+  property :released
+  property :tagline
+  has_many :in, :actors, model_class: :Person, type: :ACTED_IN
+  has_many :in, :directors, model_class: :Person, type: :DIRECTED
 end
 
 class Person
   include Neo4j::ActiveNode
   id_property :name
-  has_many :out, :acted_in, model_class: :Movie, type: :ACTED_IN, rel_class: Engagement
+  has_many :out, :acted_in, model_class: :Movie, type: :ACTED_IN
+  has_many :out, :directed, model_class: :Movie, type: :DIRECTED
 end
 
