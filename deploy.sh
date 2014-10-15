3536a44e32e647996a7347538b34d9b32d7f639f
Michael Hunger <github@jexp.de>
2014-10-15 11:17:53 +0200

    updated docs link

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,14 @@ rm -rf deploy
 mkdir deploy
 
 export IMAGE_BASE_URL=http://dev.assets.neo4j.com.s3.amazonaws.com/wp-content/uploads/
-export GITHUB={github}/
+export GITHUB=https://github.com/neo4j-contrib/developer-resources/tree/gh-pages
+export MANUAL=http://neo4j.com/docs/stable
 
-for file in `find . -mindepth 2 -name "*.adoc"`; do
+for file in `find . -mindepth 2 -name -maxdepth 3 "*.adoc"`; do
    echo "Rendering $file"
    filename=${file##*/}
    filename=${filename%.adoc}
-   bundle exec asciidoctor -a source-highlighter=codemirror -a linkattrs -a img=${IMAGE_BASE_URL} -a github="${GITHUB}" -T _templates/wordpress $file -o deploy/${filename}.html
+   bundle exec asciidoctor -a source-highlighter=codemirror -a linkattrs -a img=${IMAGE_BASE_URL} -a manual="${MANUAL}" -a github="${GITHUB}" -T _templates/wordpress $file -o deploy/${filename}.html 2>&1 | grep -v "out of sequence"
 done
 
 for guide in deploy/* ; do
--- render.sh
+++ render.sh
@@ -1,16 +1,17 @@
-export GITHUB={github}/
+export GITHUB=https://github.com/neo4j-contrib/developer-resources/tree/gh-pages
+export MANUAL=http://neo4j.com/docs/stable
 
 function render {
    file="$1"
    to=${file%/*}/index.html
    echo "Rendering $file to $to"
-   asciidoctor -a source-highlighter=codemirror -a linkattrs -a img=./ -a github="${GITHUB}" -T _templates $file -o $to
+   asciidoctor -a source-highlighter=codemirror -a linkattrs -a img=./ -a manual="${MANUAL}" -a github="${GITHUB}" -T _templates $file -o $to 2>&1 | grep -v "out of sequence"
 }
 
 if [ "$1" != "" ]; then
    render "$1"	
 else
-   for file in `find . -mindepth 2 -name "*.adoc"`; do
+   for file in `find . -mindepth 2 -maxdepth 3 -name "*.adoc"`; do
     	render "$file"
    done
 fi
