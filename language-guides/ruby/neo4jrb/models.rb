286e5bb61830afb243f1f6035eff40e5523c8b17
Brian Underwood <ml+github@semi-sentient.com>
2015-04-10 20:21:08 +0200

    Update neo4j.rb project to work with Neo4j 2.2 and do some cleanup

--- language-guides/ruby/neo4jrb/models.rb
+++ language-guides/ruby/neo4jrb/models.rb
@@ -11,14 +11,14 @@ class Movie
   id_property :title
   property :released
   property :tagline
-  has_many :in, :actors, model_class: :Person, rel_class: Engagement
+  has_many :in, :actors, model_class: :Person, rel_class: 'Engagement'
   has_many :in, :directors, model_class: :Person, type: :DIRECTED
 end
 
 class Person
   include Neo4j::ActiveNode
   id_property :name
-  has_many :out, :acted_in, model_class: :Movie, rel_class: Engagement
+  has_many :out, :acted_in, model_class: :Movie, rel_class: 'Engagement'
   has_many :out, :directed, model_class: :Movie, type: :DIRECTED
 end
 
