1c0722294838cf2b2c4eab95481640bb979dda74
Julian Simpson <simpsonjulian@gmail.com>
2015-09-25 09:54:08 +1200

    Fix the links to the guides, not just the accordion
    I introduced a bug yesterday.  This fixes the bug, tests it and makes the regex more readable (we still got 2 problems).

--- html_transformer.rb
+++ html_transformer.rb
@@ -1,11 +1,13 @@
 class HtmlTransformer
   def self.transform(lines)
-    transformed_lines = lines.select do |line|
-      line.gsub(/href="(?:\/developer\/)?(?:(?:\.\.|[a-zA-Z0-9_-]+)\/)*([^#:]+?)"/, 'href="/developer/\1"')
+    transformed_lines = lines.map do |line|
+      line.gsub(%r{href="(?:/developer/)? # base of the url
+                  (?:(?:\.\.|[a-zA-Z0-9_-]+)/)*  # the classification that we want to bin
+                  ([^#:]+?)"}x,'href="/developer/\1"')
           .gsub(/\/developer\/+developer/, '/developer')
           .gsub(/\/developer\/+(docs|graph-academy|graphacademy|editions|download|use-cases|online-course|online-training|blog|books|hardware-sizing|support|learning-neo4j-book)/, '/\1')
     end
-    return transformed_lines.join
+    return transformed_lines.join("\n")
   end
 end
 
--- spec/transformer_spec.rb
+++ spec/transformer_spec.rb
@@ -12,12 +12,24 @@ RSpec.describe HtmlTransformer do
     expect(HtmlTransformer.transform(fragment)).to match('<a href=\"/developer/cypher\">')
   end
 
-  it "renders links with a trailing slash correctly" do 
+  it "renders links with a trailing slash correctly" do
     fragment = [%{<dd class="accordion-navigation active">},
                 %{<a href="/developer/language-guides/">},
                 %{<h5>Language Guides</h5>},
                 %{ </a> }]
     expect(HtmlTransformer.transform(fragment)).to match('<a href=\"/developer/language-guides/">')
+
+  end
+
+  it "loses the section from a sidebar link" do
+    fragment = [
+        %{<li><a href="/developer/in-production/guide-cloud-deployment/">Guide: Cloud Deployment</a></li>},
+        %{<li><a href="/developer/in-production/guide-sizing-and-hardware-calculator/">Sizing + Hardware Calculator</a></li>},
+        %{<li><a href="/developer/in-production/guide-performance-tuning/">Performance Tuning</a></li>},
+        %{<li><a href="/developer/in-production/guide-clustering-neo4j/">Clustering Neo4j </a></li>}]
+    transformed_fragment = HtmlTransformer.transform(fragment)
+    puts transformed_fragment
+    expect(transformed_fragment).to match ('/developer/guide-clustering-neo4j')
 
   end
 end
