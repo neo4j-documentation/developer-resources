86fa5a4debdc16645a8c60a980619c01a2d2088b
Brian Underwood <ml+github@semi-sentient.com>
2016-01-18 18:30:34 +0700

    Making this script easier to read in less width.  Also parallelizing asciidoc rendering

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,23 @@ export GITHUB=https://github.com/neo4j-contrib/developer-resources/tree/gh-pages
 export MANUAL=http://neo4j.com/docs/stable
 export EXAMPLES=https://github.com/neo4j-examples
 
+i = 1
 for file in `find . -mindepth 2 -maxdepth 4 -name "*.adoc"`; do
-   echo "Rendering $file"
-   filename=${file##*/}
-   filename=${filename%.adoc}
-   bundle exec asciidoctor -a allow-uri-read -a source-highlighter=codemirror -a linkattrs -a img=${IMAGE_BASE_URL} -a examples=${EXAMPLES} -a manual="${MANUAL}" -a github="${GITHUB}" -T _templates/wordpress $file -o deploy/${filename}.html 2>&1 | grep -v "out of sequence"
+  echo "Rendering $file"
+  filename=${file##*/}
+  filename=${filename%.adoc}
+  bundle exec asciidoctor \
+    -a allow-uri-read,source-highlighter=codemirror,linkattrs,img=${IMAGE_BASE_URL},examples=${EXAMPLES},manual="${MANUAL}",github="${GITHUB}" \
+    -T _templates/wordpress $file \
+    -o deploy/${filename}.html 2>&1 \
+      | grep -v "out of sequence" &
+
+  ((i = i + 1))
+  # Put the forking operator at the end of the asciidoctor line
+  # This is here so that we only fork 4 at a time
+  if ! ((i % 4)); then
+    wait
+  fi
 done
 
-for guide in deploy/* ; do
-  ./publish.rb $guide
-done
+./publish.rb deploy/*.html
