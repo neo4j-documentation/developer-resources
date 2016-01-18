361572dc8e0bfc8e6f58ad6bfc2bedff5da20f28
Brian Underwood <ml+github@semi-sentient.com>
2016-01-18 18:31:54 +0700

    Various refactors, including speeding things up because we only need to make the getPosts query once for all of the files

--- publish.rb
+++ publish.rb
@@ -1,16 +1,18 @@
 #!/usr/bin/env ruby
 require 'rubygems'
 require 'bundler/setup'
+require 'logger'
 
 require_relative('html_transformer')
 require_relative('word_press_syncer')
 
-syncer = WordPressSyncer.new(ENV['BLOG_HOSTNAME'], ENV['BLOG_USERNAME'], ENV['BLOG_PASSWORD'])
+logger = Logger.new(STDOUT)
+syncer = WordPressSyncer.new(ENV['BLOG_HOSTNAME'], ENV['BLOG_USERNAME'], ENV['BLOG_PASSWORD'], logger: logger)
 
 raise 'Usage: feed me html files' if ARGV.empty?
 
 def get_value(name, lines)
-  lines.find { |l| l.match("^#{name}:.*") }.to_s.split(/:/).last.strip
+  lines.find { |l| l.match("^#{name}:.*") }.to_s.split(/:/)[1].to_s.strip
 end
 
 ARGV.each do |html_file|
@@ -30,10 +32,10 @@ ARGV.each do |html_file|
 
   # puts "data: #{data.inspect}"
 
-  @logger.info "publishing: #{post_name}"
+  logger.info "publishing: #{data[:post_name]}"
 
   syncer.sync(data[:title], data[:post_name], html,
-              [{key: 'developer_section_name', value: developer_section_name},
+              [{key: 'developer_section_name', value: data[:developer_section_name]},
                {key: 'developer_section_slug', value: ''}]) # was developer_section_slug
 end
 
--- word_press_syncer.rb
+++ word_press_syncer.rb
@@ -7,23 +7,23 @@ class WordPressSyncer
     @wp_client = Rubypress::Client.new(host: @blog_id, username: username, password: password)
     @post_type = options[:post_type] || 'developer'
     @logger = options[:logger] || Logger.new(STDOUT)
+
+    @all_pages = @wp_client.getPosts(filter: {post_type: @post_type, number: 1000})
+    @logger.info "Got #{@all_pages.length} pages from the database"
   end
 
-  def sync(title, name, html, custom_fields = {})
+  def sync(title, post_name, html, custom_fields = {})
     content = {
                 post_type:     @post_type,
                 post_date:     Time.now - 60*60*24*30,
                 post_content:  html,
                 post_title:    title,
-                post_name:     name,
+                post_name:     post_name,
                 post_status:   'publish',
-                custom_fields:  custom_fields
+                custom_fields: custom_fields
               }
 
-    all_pages = @wp_client.getPosts(filter: {post_type: @post_type, number: 1000})
-
-    @logger.info "Got #{all_pages.length} pages from the database"
-    pages = all_pages.select { |page| page['post_name'] == post_name }
+    pages = @all_pages.select { |page| page['post_name'] == post_name }
     @logger.info "Got #{pages.length} pages matching the post_name '#{post_name}'"
 
     page = pages.sort_by {|hash| hash['post_id'].to_i }.first
@@ -31,21 +31,28 @@ class WordPressSyncer
     if page
       if page['custom_fields']
         content[:custom_fields].each do |f|
-          found = page['custom_fields'].find{ |field| field['key']==f[:key] }
+          found = page['custom_fields'].find { |field| field['key'] == f[:key] }
           f['id'] = found['id'] if found
         end
       end
+
       post_id = page['post_id'].to_i
 
       @logger.info "Editing #{post_id} on _#{@blog_id}_ custom-field #{content[:custom_fields].inspect}"
 
-      puts "@wp_client.editPost(blog_id: #{@blog_id}, post_id: #{post_id}, content: #{content})"
-      # raise 'edit failed' unless @wp_client.editPost(blog_id: @blog_id, post_id: post_id, content: content)
+      send_message(:editPost, blog_id: @blog_id, post_id: post_id, content: content)
     else
       @logger.info "Making a new post for '#{title}' on _#{@blog_id}_"
 
-      puts "@wp_client.newPost(blog_id: #{@blog_id}, content: #{content})"
-      # raise 'publish failed' unless @wp_client.newPost(blog_id: @blog_id, content: content)
+      send_message(:newPost, blog_id: @blog_id, content: content)
+    end
+  end
+
+  private
+
+  def send_message(message, *args)
+    @wp_client.send(message, *args).tap do |result|
+      raise "WordPress #{message} failed!" if !result
     end
   end
 end
