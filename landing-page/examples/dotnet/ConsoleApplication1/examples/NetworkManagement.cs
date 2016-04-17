228b22d5d89135339de3f4705fa6b9157b8ce624
Michael Hunger <github@jexp.de>
2016-04-17 03:21:28 +0200

    Added Landing Page Examples for DotNet thanks @zhenlineo

---
+++ landing-page/examples/dotnet/ConsoleApplication1/examples/NetworkManagement.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Neo4j.Driver;
+
+class NetworkManagement
+{
+    public static void NetworkManagementMain(string[] args)
+    {
+        using (var driver = GraphDatabase.Driver(new Uri("bolt://localhost:7687"), AuthTokens.Basic("neo4j", "neo4j")))
+        {
+            var data = new List<string[]>
+            {
+                new[] {"CRM", "Database VM"},
+                new[] {"Database VM", "Server 2"},
+                new[] {"Server 2", "SAN"},
+                new[] {"Server 1", "SAN"},
+                new[] {"Webserver VM", "Server 1"},
+                new[] {"Public Website", "Webserver VM"},
+                new[] {"Public Website", "Webserver VM"}
+            };
+            const string loadCypher =
+                "UNWIND {servers} AS row MERGE(s1: Service { name: row[0]}) MERGE(s2: Service { name: row[1]}) MERGE(s1) -[:DEPENDS_ON]->(s2)";
+            using (var session = driver.Session())
+            {
+                var result = session.Run(loadCypher, new {servers = data});
+                result.ToList();
+            }
+
+            //Impact Analysis
+            //Find all upstreams impacted by outage of Server 1.
+            using (var session = driver.Session())
+            {
+                var result = session.Run(
+                    "MATCH (n:Service)<-[:DEPENDS_ON *]-(dependent:Service) " +
+                    "WHERE n.name = 'Server 1' " +
+                    "RETURN dependent.name AS name");
+                Console.WriteLine("Impacted by outage of Server 1");
+                foreach (var record in result)
+                {
+                    Console.WriteLine($"\t{record["name"]}");
+                }
+            }
+
+            //Dependency Analysis
+            //Find all dependencies of the public website.
+            using (var session = driver.Session())
+            {
+                var result = session.Run(
+                    "MATCH (n:Service)-[:DEPENDS_ON *]->(downstream) " +
+                    "WHERE n.name = 'Public Website' " +
+                    "RETURN downstream.name AS name");
+                Console.WriteLine("Dependencies of Public Website");
+                foreach (var record in result)
+                {
+                    Console.WriteLine($"\t{record["name"]}");
+                }
+            }
+
+            //Statistics
+            //Find the most depended-upon component.
+            using (var session = driver.Session())
+            {
+                var result = session.Run(
+                    "MATCH (n:Service)<-[:DEPENDS_ON*]-(dependent:Service) " +
+                    "RETURN n.name AS service, count(DISTINCT dependent) AS dependents " +
+                    "ORDER BY dependents DESC " +
+                    "LIMIT 1"
+                    );
+                Console.WriteLine($"Most depended-upon component is {result.Single()["service"].As<string>()}");
+            }
+        }
+    }
+}
---
+++ landing-page/examples/dotnet/ConsoleApplication1/examples/Social.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Neo4j.Driver;
+
+class SocialProgram
+{
+    private static void SocialMain(string[] args)
+    {
+        using (var s = new Social())
+        {
+            s.Setup();
+
+            var friendsOfFriends = s.FriendsOfAFriend("Joe");
+            Console.WriteLine($"Joe's friends (of friends)");
+            foreach (var fof in friendsOfFriends)
+            {
+                Console.WriteLine($"\t{fof["foaf"].As<INode>()["name"].As<string>()}");
+            }
+
+            Console.WriteLine();
+
+            var commonFriends = s.CommonFriends("Joe", "Sally");
+            Console.WriteLine("Joe and Sally's common friends");
+            foreach (var friend in commonFriends)
+            {
+                Console.WriteLine($"\t{friend["friend"].As<string>()}");
+            }
+
+            Console.WriteLine();
+
+            var connectingNames = s.ConnectingPaths("Joe", "Billy");
+            Console.WriteLine("Path to Billy");
+            foreach (var record in connectingNames)
+            {
+                var path = record["path"].As<IPath>();
+                foreach (var friend in path.Nodes)
+                {
+                    Console.WriteLine($"\t{friend["name"].As<string>()}");
+                }
+            }
+        }
+    }
+}
+
+public class Social :IDisposable
+{
+    private readonly Driver _driver;
+
+    public Social()
+    {
+        _driver = GraphDatabase.Driver(new Uri("bolt://localhost:7687"), AuthTokens.Basic("neo4j", "neo4j"));
+    }
+
+    public void Setup()
+    {
+        var peopleList = new List<string[]>
+        {
+            new[] {"Jim", "Mike"}, new[] {"Jim", "Billy"}, new[] {"Anna", "Jim"},
+            new[] {"Anna", "Mike"}, new[] {"Sally", "Anna"}, new[] {"Joe", "Sally"},
+            new[] {"Joe", "Bob"}, new[] {"Bob", "Sally"}
+        };
+
+        using (var session = _driver.Session())
+        {
+            var result = session.Run(
+                "UNWIND {people} AS pair " +
+                "MERGE (u1:Person { name: pair[0] }) " +
+                "MERGE (u2:Person { name: pair[1] }) " +
+                "MERGE (u1)-[:KNOWS]->(u2)", new {people = peopleList});
+            result.ToList();
+        }
+    }
+
+    public List<IRecord> FriendsOfAFriend(string personName)
+    {
+        using (var session = _driver.Session())
+        {
+            var result = session.Run(
+                "MATCH (p:Person)-[:KNOWS]-(friend)-[:KNOWS]-(foaf)" +
+                "WHERE p.name = {p1}" +
+                "AND NOT (p) -[:KNOWS]-(foaf)" +
+                "RETURN foaf", 
+                new {p1 = personName});
+
+            return result.ToList();
+        }
+    }
+
+    public List<IRecord> CommonFriends(string personName1, string personName2)
+    {
+        using (var session = _driver.Session())
+        {
+            var result = session.Run(
+                "MATCH (p:Person)-[:KNOWS]-(friend)-[:KNOWS]-(foaf:Person)" +
+                "WHERE p.name = {p1}" +
+                "AND foaf.name = {p2}" +
+                "RETURN friend.name AS friend",
+                new { p1 = personName1, p2 = personName2 });
+
+            return result.ToList();
+        }
+    }
+
+    public List<IRecord> ConnectingPaths(string personName1, string personName2)
+    {
+        using (var session = _driver.Session())
+        {
+            var result = session.Run(
+                "MATCH path = shortestPath((p1:Person)-[:KNOWS*..6]-(p2:Person))" +
+                "WHERE p1.name = {p1}" +
+                "AND p2.name = {p2}" +
+                "RETURN path",
+                new { p1 = personName1, p2 = personName2 });
+
+            return result.ToList();
+        }
+    }
+
+    public void Dispose()
+    {
+        _driver?.Dispose();
+    }
+}
