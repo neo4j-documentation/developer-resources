959219020c4935efe4a0bd929eea72d2a592b54d
Michael Hunger <github@jexp.de>
2016-04-26 17:41:02 +0100

    rendering

--- landing-page/examples/java/Fraud.java
+++ landing-page/examples/java/Fraud.java
@@ -1,76 +1,71 @@
+// javac -cp neo4j-java-driver*.jar:. Fraud.java
+// java -cp neo4j-java-driver*.jar:. Fraud
 
-// javac Fraud.java
-// java -cp /path/to/neo4j-jdbc-2.3-SNAPSHOT-jar-with-dependencies.jar:. Fraud
+import org.neo4j.driver.v1.*;
+import static org.neo4j.driver.v1.Values.parameters;
 
-import java.sql.*;
+import java.util.List;
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonMap;
 
 public class Fraud {
 
-    public static void query(Connection con, 
-                            String query, String[] columns, Object...params)
-                      throws SQLException {
-        try (PreparedStatement pst = con.prepareStatement(query)) {
-            for (int i=0;i<params.length;i++) 
-                pst.setObject(i + 1, params[i]);
-            ResultSet rs = pst.executeQuery();
-            while (rs.next()) {
-                for (int i=0;i<columns.length;i++) 
-                    System.out.print(rs.getObject(columns[i])+"\t");
-                System.out.println();
-            }
-        }
-    }
-    public static void main(String...args) throws SQLException {
-        Connection con = DriverManager
-             .getConnection("jdbc:neo4j://localhost:7474/","neo4j","<password>");
+    public static void main(String...args) {
 
-        String insertQuery = 
-            "CREATE (hank:Person {name:'Hank'})," +
-            "(abby:Person {name:'Abby'})," +
-            "(max:Person {name:'Max'})," +
-            "(sophie:Person {name: 'Sophie'})," +
-            "(jane:Person {name: 'Jane'})," +
-            "(bill:Person {name: 'Bill'})," +
-            "(ssn993632634:SSN {number: 993632634})," +
-            "(ssn123456789:SSN {number: 123456789})," +
-            "(ssn523252364:SSN {number: 523252364})," +
-            "(chase:Account {bank: 'Chase', number: 1523})," +
-            "(bofa:Account {bank: 'Bank of America', number: 4634})," +
-            "(cayman:Account {bank: 'Cayman', number: 863})," +
-            "(bill)-[:HAS_SSN]->(ssn523252364)," +
-            "(bill)-[:HAS_ACCOUNT]->(bofa)," +
-            "(jane)-[:HAS_SSN]->(ssn123456789)," +
-            "(jane)-[:HAS_ACCOUNT]->(chase)," +
-            "(hank)-[:HAS_ACCOUNT]->(cayman)," +
-            "(abby)-[:HAS_ACCOUNT]->(cayman)," +
-            "(abby)-[:HAS_SSN]->(ssn993632634)," +
-            "(sophie)-[:HAS_SSN]->(ssn993632634)," +
-            "(max)-[:HAS_SSN]->(ssn993632634)";
-        try {
-            PreparedStatement pst = con.prepareStatement(insertQuery);
-            pst.executeUpdate();
-            pst.close();
-    
+        Config noSSL = Config.build().withEncryptionLevel(Config.EncryptionLevel.NONE).toConfig();
+        Driver driver = GraphDatabase.driver("bolt://localhost",AuthTokens.basic("neo4j","test"),noSSL); // <password>
+        try (Session session = driver.session()) {
+
+            String insertQuery = 
+                "CREATE (hank:Person {name:'Hank'})," +
+                "(abby:Person {name:'Abby'})," +
+                "(max:Person {name:'Max'})," +
+                "(sophie:Person {name: 'Sophie'})," +
+                "(jane:Person {name: 'Jane'})," +
+                "(bill:Person {name: 'Bill'})," +
+                "(ssn993632634:SSN {number: 993632634})," +
+                "(ssn123456789:SSN {number: 123456789})," +
+                "(ssn523252364:SSN {number: 523252364})," +
+                "(chase:Account {bank: 'Chase', number: 1523})," +
+                "(bofa:Account {bank: 'Bank of America', number: 4634})," +
+                "(cayman:Account {bank: 'Cayman', number: 863})," +
+                "(bill)-[:HAS_SSN]->(ssn523252364)," +
+                "(bill)-[:HAS_ACCOUNT]->(bofa)," +
+                "(jane)-[:HAS_SSN]->(ssn123456789)," +
+                "(jane)-[:HAS_ACCOUNT]->(chase)," +
+                "(hank)-[:HAS_ACCOUNT]->(cayman)," +
+                "(abby)-[:HAS_ACCOUNT]->(cayman)," +
+                "(abby)-[:HAS_SSN]->(ssn993632634)," +
+                "(sophie)-[:HAS_SSN]->(ssn993632634)," +
+                "(max)-[:HAS_SSN]->(ssn993632634)";
+          
+            session.run(insertQuery,parameters()).consume();
+          
+            StatementResult result;
+            
             String transitiveQuery = 
-            " MATCH (n:Person)-[*]-(o) " +
-            " WHERE n.name = {1} "+
-            " RETURN o ";
-            query(con, transitiveQuery, new String[] {"o"}, "Hank");
+                " MATCH (n:Person)-[*]-(o) " +
+                " WHERE n.name = {name} "+
+                " RETURN o ";
 
+            result = session.run(transitiveQuery, parameters("name","Hank"));
+            while (result.hasNext()) System.out.println(result.next().get("o").asMap());
+      
             String targetingQuery =
-            "MATCH (n:Person)-[*]-(o) " +
-            " WITH n, count(DISTINCT o) AS size " +
-            " WHERE size > 2 " +
-            " RETURN n";
-            query(con, targetingQuery, null);
+                "MATCH (n:Person)-[*]-(o) " +
+                " WITH n, count(DISTINCT o) AS size " +
+                " WHERE size > 2 " +
+                " RETURN n";
 
+            result = session.run(targetingQuery, parameters());
+            while (result.hasNext()) System.out.println(result.next().get("n").asMap());
+      
             String insightsQuery =
-            "MATCH (ssn:SSN)<-[:HAS_SSN]-(:Person)-[:HAS_ACCOUNT]->(acct:Account) "+
-            " WHERE ssn.number = {1} "+
-            " RETURN acct";
-            query(con, insightsQuery, new String[] {"acct"}, 993632634);
-        } finally {
-            con.close();
+                "MATCH (ssn:SSN)<-[:HAS_SSN]-(:Person)-[:HAS_ACCOUNT]->(acct:Account) "+
+                " WHERE ssn.number = {ssn} "+
+                " RETURN acct";
+            result = session.run(insightsQuery, parameters("ssn",993632634));
+            while (result.hasNext()) System.out.println(result.next().get("acct").asMap());
         }
     }
 }
--- landing-page/examples/java/test.sh
+++ landing-page/examples/java/test.sh
@@ -1,9 +1,8 @@
 echo "You have to have a Neo4j server running with neo4j:<password> set as credentials"
 
-export CLASSPATH=neo4j-java-driver*.jar:.
+export CLASSPATH=neo4j-java-driver-1.0.0.jar:.
+rm *.class
 javac [SFN]*.java
 java -cp $CLASSPATH Social
 java -cp $CLASSPATH Fraud
 java -cp $CLASSPATH Network
-
-
