9b7efea1ec64e8abe1573148a584816fe7320eb7
Ryan Boyd <ryan@ryguy.com>
2018-08-03 14:12:22 -0700

    regex appears to be causing more problems than it fixes, but the purpose is a bit unknown

--- html_transformer.rb
+++ html_transformer.rb
@@ -4,12 +4,12 @@ module HtmlTransformer
 
     lines.map do |line|
       line
-          .gsub(%r{href="/developer/}, 'href="/developer/')
-          .gsub(%r{href="(?:/developer/)? # base of the url
-                  (?:(?:\.\.|[a-zA-Z0-9_-]+)/)*  # the classification that we want to bin
-                  ([^#:]+?)"}x,'href="/developer/\1"')
           .gsub(%r{/developer/+developer}, '/developer')
           .gsub(%r{/developer/+(docs|graph-academy|sandbox|graphacademy|editions|download|use-cases|online-course|online-training|blog|books|hardware-sizing|support|learning-neo4j-book)}, '/\1')
+#          .gsub(%r{href="/developer/}, 'href="/developer/')
+#          .gsub(%r{href="(?:/developer/)? # base of the url
+#                  (?:(?:\.\.|[a-zA-Z0-9_-]+)/)*  # the classification that we want to bin
+#                  ([^#:]+?)"}x,'href="/developer/\1"')
     end.join("")
   end
 end
